b735370a1104fa17a45d7a4f0cf5f437ddb6cbe1
Dias <wprogrammist@gmail.com>
2022-12-12 05:58:17 +0600

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ function MainPage() {
 	  let contractAddress = '0xCF31E7c9E7854D7Ecd3F3151a9979BC2a82B4fe3';
 
 	
-	const [defaultAccount, setDefaultAccount] = React.useState<null | string>(null);
+	const [address, setDefaultAccount] = React.useState<null | string>(null);
 	const [user, setUser] = React.useState<null | User>(null);
 
 	const [currentContractVal, setCurrentContractVal] = React.useState(null);
@@ -154,6 +154,15 @@ function MainPage() {
       }
     }
     
+
+    const removeMe = () => {
+      let arr = data || [];
+      arr.shift();
+      setData(JSON.parse(JSON.stringify(arr)));
+      localStorage.removeItem('user');
+      setUser(null);
+      window.location.reload();
+    }
     
     
     React.useEffect(() => {
@@ -218,7 +227,16 @@ function MainPage() {
     }
     <div className={"App" + (!hasExtension ? ' pointless' : '')}>
     
-    <header className='in-row'>
+   
+      <svg width="0" height="0" viewBox="0 0 320 322" fill="none" xmlns="http://www.w3.org/2000/svg" >
+          <clipPath id="mask" style={{position: 'absolute', right: 0}}>
+            <rect width="320" height="322" rx="160" fill="#E75626" style={{position: 'absolute', right: 0}}/>
+          </clipPath>
+      </svg>
+
+
+      <div className="full-screen">
+      <header className='in-row'>
         <div className="in-row half-width content-left">
           <Link to="/">
             <div className="logo alignment-center">
@@ -230,23 +248,12 @@ function MainPage() {
         </div>
       <div className="in-row half-width content-right">
           {
-          defaultAccount ? <span className='primary-color'>{defaultAccount.slice(0, 15)}...</span> : <button className="wallet primary-bg slim-width secondary-color" onClick={connectWalletHandler}>
+          address ? <span className='primary-color'>{address.slice(0, 15)}...</span> : <button className="wallet primary-bg slim-width secondary-color" onClick={connectWalletHandler}>
             Connect metamask
           </button>
           }
       </div>
       </header>
-
- 
-    
-      <svg width="0" height="0" viewBox="0 0 320 322" fill="none" xmlns="http://www.w3.org/2000/svg" >
-          <clipPath id="mask" style={{position: 'absolute', right: 0}}>
-            <rect width="320" height="322" rx="160" fill="#E75626" style={{position: 'absolute', right: 0}}/>
-          </clipPath>
-      </svg>
-
-
-      <div className="full-screen">
         <div className='from-bottom' style={{height: '100%'}}>
         <div className="in-row from-bottom" style={{
               marginBottom: '60px'
@@ -297,8 +304,8 @@ function MainPage() {
                             <div className="quarter">
                               <p>Q1 2022</p>
                             </div>
-                            <span className="sub-circle">
-                              <span className="point point-1"></span>
+                            <span className="point point-1">
+                              <span className="sub-circle"></span>
                             </span>
                             <span className="point point-2"></span>
                             <span className="point point-3"></span>
@@ -352,18 +359,20 @@ function MainPage() {
                 e.preventDefault()
                 var formData = new FormData(e.target as HTMLFormElement);
                 console.log('formData :', formData.get('username'), formData.get('email'));
-                  if(defaultAccount && !user){
-                    let user: User = {
-                      id: null,
-                      username: formData.get('username') as string,
-                      email: formData.get('email') as string,
-                      address: defaultAccount
+                  if(address && !user){
+                    if((formData.get('username') as string).trim() !== '' && (formData.get('email')  as string).trim() !== ''){
+                      let user: User = {
+                        id: null,
+                        username: formData.get('username') as string,
+                        email: formData.get('email') as string,
+                        address: address
+                      }
+                      let arr = JSON.parse(JSON.stringify(data)) || []
+                      arr.unshift(user)
+                      setUser(user)
+                      localStorage.setItem('user', JSON.stringify(user))
+                      setData(arr)
                     }
-                    let arr = JSON.parse(JSON.stringify(data)) || []
-                    arr.unshift(user)
-                    setUser(user)
-                    localStorage.setItem('user', JSON.stringify(user))
-                    setData(arr)
                   }else {
                     connectWalletHandler()
                   }
@@ -371,29 +380,29 @@ function MainPage() {
                 <div className="user__field">
                   <p className='secondary-color user__label'>Name</p>
                   {
-                    (defaultAccount && user) ?  <p className='primary-color user__form-value'>{user.username}</p> : <input className='user__form-input primary-color' type="text" placeholder='We will display your name in participation list' name='username' />
+                    (address && user) ?  <p className='primary-color user__form-value'>{user.username}</p> : <input className='user__form-input primary-color' type="text" placeholder='We will display your name in participation list' name='username' />
                   }
                   
                 </div>
                 <div className="user__field">
                   <p className='secondary-color user__label'>Email</p>
                   {
-                    (defaultAccount && user) ?  <p className='primary-color user__form-value'>
+                    (address && user) ?  <p className='primary-color user__form-value'>
                       {user.email}
                     </p> : <input className='user__form-input primary-color' type="text" placeholder='We will display your email in participation list' name='email' />
                   }
                 </div>
                 <button className={"user__form-button primary-bg secondary-color"+ (listMe ? ' pale': '')} onClick={()=> {
-                  if(defaultAccount && user && !listMe){
+                  if(address && user && !listMe){
                     setListMe(true)
                   }
                 }}>
-                  { (defaultAccount && user) ? 'List me to the table' : 'Connect metamask'}
+                  { (address && user) ? 'List me to the table' : 'Connect metamask'}
                 </button>
               </form>
           </div>
           {
-            (defaultAccount && user) ?  
+            (address && user) ?  
             <div className="slim">
               <p className="caption">
                 Participation listing (enable only for participants)
@@ -420,10 +429,21 @@ function MainPage() {
                           </Link>
                         </td>
                         {/* <td><span className="slice">{item.address}</span></td> */}
-                        <td className={!item.id ? 'pointless': ''}>
+                        <td className={!item.id ? 'td3 parent': ''}>
                           <Link to={"/user-page/"+item.id}>
                               {item.address.slice(0, 20)}...
                           </Link>
+
+                          {
+                            !item.id && listMe ? 
+                            <svg className='remove-me flow'
+                            onClick={removeMe}
+                            width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect x="2.11035" y="2.81799" width="1" height="10" rx="0.5" transform="rotate(-45 2.11035 2.81799)" fill="white"/>
+                            <rect x="2.81738" y="9.88904" width="1" height="10" rx="0.5" transform="rotate(-135 2.81738 9.88904)" fill="white"/>
+                            </svg> : ''
+                          }
+
                         </td>
                     </tr>
                   ))}
@@ -459,7 +479,8 @@ function UserPage(){
   return (
     <>
     <div className="App">
-    <header className='in-row'>
+       <div className="full-screen">
+        <header className='in-row'>
         <div className="in-row half-width content-left">
           <Link to="/">
             <div className="logo alignment-center">
@@ -472,53 +493,54 @@ function UserPage(){
       <div className="in-row half-width content-right">
         <span className='primary-color'>{localUser?.address.slice(0, 15)}...</span>
       </div>
-      </header>
-       <div className="full-screen parent v-center">
-        <div className="in-row info">
-          <div className='press user'>
-            <h1 className="user__header secondary-color">
-              Personal data
-            </h1>
-            <div className="user__form">
-              <div className="user__field">
-                <p className='secondary-color user__label'>Name</p>
-                <p className='primary-color user__form-value'>
-                  {user?.username}
-                </p>
-              </div>
-              <div className="user__field">
-                <p className='secondary-color user__label'>Email</p>
-                <p className='primary-color user__form-value'>
-                  {user?.email}
-                </p>
-              </div>
-              <div className="user__field">
-                <p className='secondary-color user__label'>Wallet</p>
-                <p className='primary-color user__form-value'>
-                  {user?.address}
-                </p>
+        </header>
+        <div className='parent v-center fill'>
+          <div className="in-row info">
+            <div className='press user'>
+              <h1 className="user__header secondary-color">
+                Personal data
+              </h1>
+              <div className="user__form">
+                <div className="user__field">
+                  <p className='secondary-color user__label'>Name</p>
+                  <p className='primary-color user__form-value'>
+                    {user?.username}
+                  </p>
+                </div>
+                <div className="user__field">
+                  <p className='secondary-color user__label'>Email</p>
+                  <p className='primary-color user__form-value'>
+                    {user?.email}
+                  </p>
+                </div>
+                <div className="user__field">
+                  <p className='secondary-color user__label'>Wallet</p>
+                  <p className='primary-color user__form-value'>
+                    {user?.address}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="fifth-level circle flow">
-            <div className="circle__" style={{pointerEvents: 'none'}}>
-              <div className="circle__item">
-                <div className="circle__half circle__half--clipped"></div>
-              </div>
-              <div className="circle__half circle__half--fix"></div>
-              <div className="fourth-level circle slim-width">
-                <div className="third-level circle slim-width">
-                  <div className="second-level circle slim-width">
-                    <div className="first-level circle slim-width">
-                      <div className='parent'>
-                          <img className='pointless' src={planet} alt=""/>
+            <div className="fifth-level edge circle flow">
+              <div className="circle__" style={{pointerEvents: 'none'}}>
+                <div className="circle__item">
+                  <div className="circle__half circle__half--clipped"></div>
+                </div>
+                <div className="circle__half circle__half--fix"></div>
+                <div className="fourth-level circle slim-width">
+                  <div className="third-level circle slim-width">
+                    <div className="second-level circle slim-width">
+                      <div className="first-level circle slim-width">
+                        <div className='parent'>
+                            <img className='pointless' src={planet} alt=""/>
+                        </div>
+                        <span className="point point-1 hide">
+                          <span className="sub-circle"></span>
+                        </span>
+                        <span className="point point-2"></span>
+                        <span className="point point-3"></span>
+                        <span className="point point-4"></span>
                       </div>
-                      <span className="sub-circle">
-                        <span className="point point-1"></span>
-                      </span>
-                      <span className="point point-2"></span>
-                      <span className="point point-3"></span>
-                      <span className="point point-4"></span>
                     </div>
                   </div>
                 </div>
